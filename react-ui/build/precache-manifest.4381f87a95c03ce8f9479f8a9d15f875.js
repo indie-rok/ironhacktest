e0550c7c3b9c1fc22c5bee95b8607b82e98221d8
Emmanuel Orozco <indi3.rok@gmail.com>
2019-12-10 08:31:39 +0100

    updated routes to serve to heroku

---
+++ react-ui/build/precache-manifest.4381f87a95c03ce8f9479f8a9d15f875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd6788c4b2884be4acf0402dcaca640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ca12a63685440c4272",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8ba4efd3e8c011430a26",
+    "url": "/static/js/2.1a43ad1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1a43ad1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91ca12a63685440c4272",
+    "url": "/static/js/main.6c0aedba.chunk.js"
+  },
+  {
+    "revision": "11202ce601dcca9717de",
+    "url": "/static/js/runtime-main.28f4e309.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
