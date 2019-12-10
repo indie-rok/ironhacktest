3c1e706b874142a0046aadf8114e37c525185fb0
Emmanuel Orozco <indi3.rok@gmail.com>
2019-12-10 08:08:00 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+async function test() {
+  const res = await fetch("http://localhost:5000");
+  const json = await res.json();
+  console.log(json);
+}
 
 function App() {
+  test();
+
   return (
     <div className="App">
       <header className="App-header">
