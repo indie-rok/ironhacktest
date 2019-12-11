dd7b376753014fc726c1fc86e2e11e3d487adaf9
Emmanuel Orozco <indi3.rok@gmail.com>
2019-12-11 11:12:17 +0100

    added email auth backend logic

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const path = require("path");
-
-const isDev = process.env.NODE_ENV !== "production";
-const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+const config = require("config");
+const usersRoute = require("./routes/user.route");
 
 const app = express();
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
+//connect to mongodb
+
+mongoose
+  .connect(config.get("mongoUrl"), { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB...", err));
+
+app.use(express.json());
+
 // API
 app.get("/api", function(req, res) {
   res.set("Content-Type", "application/json");
   res.send('{"message":"Hello from the custom server!"}');
 });
+
+app.use("/api", usersRoute);
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("*", function(request, response) {
