77dc930e61a9a2aa6cfadf87071963b44832e340
Emmanuel Orozco <indi3.rok@gmail.com>
2020-01-14 21:00:36 +0100

    added logout

--- react-ui/src/GeneralComponents/Menu/index.js
+++ react-ui/src/GeneralComponents/Menu/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Nav, Navbar, Button } from "react-bootstrap";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-export default function Menu() {
+import * as emailAuthActions from "../../features/Sessions/redux/actions";
+
+function Menu({ actions }) {
   return (
     <Navbar bg="light" expand="lg">
-      <Link to="/login">IMDB clone</Link>
+      <Link to="/films">IMDB clone</Link>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav className="ml-auto">
@@ -17,9 +20,22 @@ export default function Menu() {
 
       <Navbar.Collapse>
         <Nav className="ml-auto">
-          <Button variant="default">Log out</Button>
+          <Button variant="default" onClick={actions.logout}>
+            Log out
+          </Button>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 }
+
+const mapDispatchToProps = dispatch => ({
+  actions: {
+    logout: () => {
+      dispatch(emailAuthActions.logout());
+      window.location.reload();
+    }
+  }
+});
+
+export default connect(null, mapDispatchToProps)(Menu);
--- react-ui/src/features/Sessions/redux/reducers.js
+++ react-ui/src/features/Sessions/redux/reducers.js
@@ -13,6 +13,8 @@ export const EmailAuthReducer = (state = initialState, action) => {
       return { ...state, errors: { SignIn: action.error } };
     case actions.EMAIL_AUTH_SIGNUP_SUCCESS:
       return { ...state, user: action.user };
+    case actions.EMAIL_AUTH_LOGOUT:
+      return { ...state, user: { accessToken: null } };
     case actions.EMAIL_AUTH_SIGNUP_ERROR:
       return { ...state, errors: { SignUp: action.error } };
     case actions.EMAIL_AUTH_LOGOUT:
